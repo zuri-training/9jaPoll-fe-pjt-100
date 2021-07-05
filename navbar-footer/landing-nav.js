63ce4ff746143dfdadeb1809bb133a3fa061b5f2
Yusuf Rilwan <akinyusuf5@gmail.com>
2021-07-05 12:09:22 +0100

    navbar setup

--- navbar-footer/landing-nav.js
+++ navbar-footer/landing-nav.js
@@ -2,10 +2,19 @@ const menu = document.querySelector('img.menu');
 const closer = document.querySelector('img.close');
 const mobile_nav = document.querySelector('div.mobile-nav');
 
+const toggleIcon = (delay) => {
+    setTimeout(() => {
+        menu.classList.toggle('hide-icon');
+        closer.classList.toggle('hide-icon');
+    }, delay)
+}
+
 menu.addEventListener('click', (e) => {
     mobile_nav.classList.add('show');
+    toggleIcon(200);
 });
 
 closer.addEventListener('click', (e) => {
+    toggleIcon(0);
     mobile_nav.classList.remove('show');
 })
