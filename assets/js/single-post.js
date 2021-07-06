e765f855aea25804b3e013e13d90cc3cd96ddd72
Adedunmola Adeiye <adeiyecharles@gmail.com>
2021-07-06 19:20:23 +0100

    create post page created, updated create post link in post-page.html, added footer to single-post page

--- assets/js/single-post.js
+++ assets/js/single-post.js
@@ -19,3 +19,36 @@ allLinks.forEach(link=>{
     })
 })
 
+let postContainer = document.querySelector('.card-container');
+
+async function getSimilarPosts() {
+    try {
+        let response = await fetch("https://propernaijapoll.herokuapp.com/api/v1/articles")
+        let data = await response.json();
+        let articles = data.articles;
+        console.log(articles); 
+
+        let posts = articles.map((post) => {
+            const {title, description} = post;
+            return `
+            <div class="post-card">
+            <img src="./assets/images/similar-post.png">
+            <h4>${title}</h4>
+            <p>${ description}</p>
+            </div>
+            `
+        })
+
+        postContainer.innerHTML = posts.join('')
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
+
+
+getSimilarPosts()
+
